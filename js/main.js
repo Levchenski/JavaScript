954fa2df1613b664eeeff2ad09cd59e637b3012d
Sergey <73191202+Levchenski@users.noreply.github.com>
2021-07-17 18:35:42 +0300

    0.0.55

---
+++ js/main.js
@@ -0,0 +1,38 @@
+let itemOne = document.getElementsByClassName('item')[0];
+let newobj = document.querySelectorAll('.newobj');
+
+let cvn = document.getElementsByTagName('canvas')[0];
+
+newobj[0].prepend(cvn);
+let ctx = cvn.getContext("2d");
+let x, y, w, h;
+x = 100;
+y = 50;
+w = 50;
+h = 50;
+ctx.fillStyle = 'red';
+ctx.fillRect(x, y, w, h);
+ctx.fillStyle = "blue";
+ctx.fillRect(0, y, w, h);
+ctx.clearRect(0, 0, 400, 200);
+ctx.strokeStyle = "green";
+ctx.lineWidth = "10";
+ctx.rect(50, 20, 200, 100);
+ctx.stroke();
+ctx.fillStyle = "blue";
+ctx.fill();
+
+let range = document.querySelector('[type="range"]');
+console.log(range);
+
+function calc() {
+   this.read = function () {
+      this.a = +prompt('a', 0);
+      this.b = +prompt('b', 0);
+   };
+   this.sum = function () {
+      return this.a + this.b;
+   };
+}
+
+let clacul = new calc.sum();
