e099052ac0b3af2ec48a03735ffc8814f807b318
Sergey <73191202+Levchenski@users.noreply.github.com>
2021-07-18 20:54:05 +0300

    0.0.71

--- js/main.js
+++ js/main.js
@@ -8,8 +8,36 @@ function obj(name, age, city) {
 }
 const id0 = new obj('Sergey', 23, "St.Petersburg");
 
-let = Symbol("id");
+let orId = Symbol("id");
 
 id0[orId] = 25;
 
+
+
 console.log(id0);
+
+
+const user = {
+   name: "jorg",
+   age: 20,
+   [orId]: id0,
+}
+
+console.log(user);
+
+for (key in user) {
+   console.log(user[key]);
+}
+console.log(user[orId]);
+
+let clon = Object.assign({}, user);
+
+let soc = Symbol.for('id');
+let daa = Symbol.for('id');
+
+console.log(soc === daa);
+
+console.log(clon[orId]);
+
+let o = Symbol.keyFor(soc);
+console.log(o);
