45d4caa2151c5a510c2e8e5620ebc64992fb0d19
Sergey <73191202+Levchenski@users.noreply.github.com>
2021-07-19 00:22:13 +0300

    0.0.75

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-let itemOne = document.getElementsByClassName('item')[0];
 let newobj = document.querySelectorAll('.newobj');
+
+let is = document.querySelector('[name="range"]').value;
+
+newobj[0].textContent = is;
+newobj.addEventListener("click", () => {
+   console.log(newobj[0]);
+});
 
 function obj(name, age, city) {
    this.name = name;
@@ -7,37 +13,23 @@ function obj(name, age, city) {
    this.city = city;
 }
 const id0 = new obj('Sergey', 23, "St.Petersburg");
-
 let orId = Symbol("id");
-
 id0[orId] = 25;
-
-
-
 console.log(id0);
-
-
 const user = {
    name: "jorg",
    age: 20,
    [orId]: id0,
 }
-
 console.log(user);
-
 for (key in user) {
    console.log(user[key]);
 }
-console.log(user[orId]);
-
 let clon = Object.assign({}, user);
-
 let soc = Symbol.for('id');
 let daa = Symbol.for('id');
-
-console.log(soc === daa);
-
-console.log(clon[orId]);
-
 let o = Symbol.keyFor(soc);
+console.log(user[orId]);
+console.log(soc === daa);
+console.log(clon[orId]);
 console.log(o);
