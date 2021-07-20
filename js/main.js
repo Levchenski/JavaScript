34b27a89481b328fc9a51312a07c6d5fd1e206c7
Sergey <73191202+Levchenski@users.noreply.github.com>
2021-07-20 16:13:57 +0300

    0.0.78

--- js/main.js
+++ js/main.js
@@ -1,35 +1,13 @@
 let newobj = document.querySelectorAll('.newobj');
-
 let is = document.querySelector('[name="range"]').value;
+let bis = document.querySelector('[name="range"]');
+let em = document.querySelector('[type="email"]');
 
-newobj[0].textContent = is;
-newobj.addEventListener("click", () => {
-   console.log(newobj[0]);
-});
+let a = 0;
+let b = 0;
 
-function obj(name, age, city) {
-   this.name = name;
-   this.age = age;
-   this.city = city;
-}
-const id0 = new obj('Sergey', 23, "St.Petersburg");
-let orId = Symbol("id");
-id0[orId] = 25;
-console.log(id0);
-const user = {
-   name: "jorg",
-   age: 20,
-   [orId]: id0,
-}
-console.log(user);
-for (key in user) {
-   console.log(user[key]);
-}
-let clon = Object.assign({}, user);
-let soc = Symbol.for('id');
-let daa = Symbol.for('id');
-let o = Symbol.keyFor(soc);
-console.log(user[orId]);
-console.log(soc === daa);
-console.log(clon[orId]);
-console.log(o);
+let o = a || b;
+
+let mesagge = (o) ? 'ok' : 'No';
+
+console.log(mesagge);
