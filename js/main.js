b89782b93750daaec576e78b071261333d92347e
Sergey <73191202+Levchenski@users.noreply.github.com>
2021-07-23 18:22:58 +0300

    0.0.102

--- js/main.js
+++ js/main.js
@@ -2,13 +2,3 @@ let newobj = document.querySelectorAll('.newobj');
 let is = document.querySelector('[name="range"]').value;
 let bis = document.querySelector('[name="range"]');
 let em = document.querySelector('[type="email"]');
-
-console.log(true && true);
-
-let hour = 12;
-let minute = 30;
-
-let o = (hour == 12 && minute == 30) ? 'Время в щи' : undefined;
-
-
-console.log(o);
