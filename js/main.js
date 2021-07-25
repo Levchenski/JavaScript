f192d1eb3b3259c34b02a6f174b99c869c668733
Sergey <73191202+Levchenski@users.noreply.github.com>
2021-07-25 22:12:52 +0300

    0.0.1102

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-let user = {
-    name: 'Serg',
-    age: 23,
-    city: 'St.petersburg',
+
+function Obj(name, age, city, region, sex, number, admin) {
+    this.name = name;
+    this.age = age;
+    this.city = city;
+    this.region = region;
+    this.sex = sex;
+    this.number = number;
+    this.admin = admin;
 }
 
-console.log(user.age);
+let Sergey = new Obj('Sergey', 23, 'St.petersburg', 'RU', 'M', '+79214312887', true);
+console.log(Sergey);
+
+function usNew(params) {
+    console.log(new.target);
+}
+
+new usNew();
